42e69a0f4732707733a0227370c3524183fa5195
Will Storey <will@willstorey.ca>
2020-12-06 14:49:54 -0500

    Finished day1/part1 - Removed C folder cause that got pretty whack - Finished in Node

---
+++ day1/part1/main.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const readline = require('readline');
+
+async function lineByLine() {
+    let inputArr = [];
+    const fileStram = fs.createReadStream('input.txt');
+
+    let input = readline.createInterface({
+        input: fileStram,
+        crlfDelay: Infinity
+    });
+
+    for await (const line of input) {
+        inputArr.push(parseInt(line));
+    }
+
+    inputArr.forEach((line, i) => {
+        for(j = i; j < inputArr.length; j++) {
+            let sumOfLines = line + inputArr[j];
+            if(sumOfLines == 2020) {
+                console.log(`${inputArr[i]} + ${inputArr[j]} = ${inputArr[i] + inputArr[j]} | Product is - ${inputArr[i] * inputArr[j]}`);
+            }
+        }
+    });
+}
+
+lineByLine();
